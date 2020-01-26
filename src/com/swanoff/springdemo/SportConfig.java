9c20ef8b5c7bd6b3a2a8631cedbcb719e10899c0
Surya <suryathegreat.07@gmail.com>
2020-01-26 12:50:18 +0530

    Spring Configuration with Java Code (no XML)
    SportConfig class is the Java config file. It is defined with @Configuration annotation.
    
    @ComponentScan tells where to scan for components (classes marked with @Component) by attaching the base package name.

---
+++ src/com/swanoff/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package com.swanoff.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.swanoff.springdemo")
+public class SportConfig {
+
+}
