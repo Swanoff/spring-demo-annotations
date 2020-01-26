07e331cd517c30e58b8e394fea7f1f58615b1896
Surya <suryathegreat.07@gmail.com>
2020-01-26 09:18:59 +0530

    Default bean_id provided by Spring
    After removing explicit bean id from the @Component annotation of a class, Spring assigns a default bean_id to that class.
    
    Default bean_id format eg:
    ClassName: TennisCoach -> BeanId: tennisCoach

--- src/com/swanoff/springdemo/TennisCoach.java
+++ src/com/swanoff/springdemo/TennisCoach.java
@@ -2,7 +2,7 @@ package com.swanoff.springdemo;
 
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach")
+@Component
 public class TennisCoach implements Coach {
 
 	@Override
